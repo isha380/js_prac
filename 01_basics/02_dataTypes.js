e63c280044797ba8e7dae06fa896f78532fe99e1
isha magar <140723992+isha380@users.noreply.github.com>
2024-02-06 08:19:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as never version
+// alert(10+10) we are using nodejs,not browser
+
+//primitive dataTypes
+//number
+//bigint
+//string
+//boolean => true/false 
+//null =>standalone value (empty)
+//undefined => value not defined
+//symbol =>unique (used when identifing uniqueness from components)
+
+//null is object
+
+
+//object 
+let name="isha"
+console.log(typeof name)
