d1bc6b172c2e2a8a0fe424d8c3eaa1da0f69fa8e
isha magar <140723992+isha380@users.noreply.github.com>
2024-02-10 06:25:17 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let marks="33abc";
+console.log( marks);
+console.log( typeof marks);
+let valueNumber=Number(marks);
+console.log("The convert number is",(valueNumber));
+console.log("The type of number is",(typeof valueNumber));
+ 
+let score=null;
+console.log(score);
+console.log(typeof score);
+let num=Number(score);
+console.log(num);
+console.log(typeof num);
+
+let m=true;
+let n=Number(m);
+console.log(n);
+console.log(typeof n);
+
+let a=undefined;
+let b=Number(a);
+console.log(b);
+console.log(typeof b);
+
+let logIn="isha";
+let booleanIsLogIn=Boolean(logIn);;
+console.log(booleanIsLogIn);
+// 1=>true ; 0=>false
+//""=> false
+//"isha" =>true
+
+let no=88
+let stringNumber=String(no)
+console.log(stringNumber);
+console.log(typeof stringNumber);
