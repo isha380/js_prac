e32e99b503a540fed67e690341c18e5013f6b941
isha magar <140723992+isha380@users.noreply.github.com>
2024-02-21 13:13:03 +0000

    datatypes_summary

---
+++ datatypes-summary.js
@@ -0,0 +1,8 @@
+//datatpe is categorized on the basis of how data is kept in memory and can access from it
+//primitive
+ //7 types:string, number,boolean,BigInt,null, undefined, symbol
+
+
+ //reference(non-primitive)
+
+ //array,objects,functions
