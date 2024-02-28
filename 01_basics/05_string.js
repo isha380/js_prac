4f7490520aecbee074e33a0e9ca9d2b608e114d4
isha magar <140723992+isha380@users.noreply.github.com>
2024-02-28 15:02:41 +0000

    learn basics about string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name="isha";
+const age=20;
+console.log(name + " " +age); //outdated way to write
+console.log(`hello my name is ${name}. I am ${age} `);
+const schoolName= new String('hbo ');
+console.log(schoolName.length);
+console.log(schoolName.toUpperCase());
+console.log(schoolName.charAt(4));
+console.log(schoolName.indexOf('b'));
