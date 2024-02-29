a4ce93cf04342a37be05a3bf33fb40e561b13b2f
isha magar <140723992+isha380@users.noreply.github.com>
2024-02-29 12:01:08 +0000

    learn about substring,trim,split,replace,includes

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,4 +1,4 @@
-const name="isha";
+const name="ishamagar";
 const age=20;
 console.log(name + " " +age); //outdated way to write
 console.log(`hello my name is ${name}. I am ${age} `);
@@ -7,3 +7,20 @@ console.log(schoolName.length);
 console.log(schoolName.toUpperCase());
 console.log(schoolName.charAt(4));
 console.log(schoolName.indexOf('b'));
+ 
+const newName= name.substring(0,2);
+console.log(newName);
+
+const one="   radhekrishna    ";
+console.log(one);
+console.log(one.trim());
+
+const practice="hari-hair-bol";
+console.log(practice);
+console.log(practice.replace("hair","HARI"));
+
+console.log(practice.includes("bol"));
+console.log(practice.includes("mahadev"));
+
+console.log(practice.split("i",2));
+console.log(practice.split("i",3));
