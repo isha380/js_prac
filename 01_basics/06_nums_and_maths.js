f90fa4ebbf129ff9284d1a60fb2795b144e55e7d
isha magar <140723992+isha380@users.noreply.github.com>
2024-03-27 13:30:51 +0000

    learn about numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,10 @@
+const num=100;
+console.log(num);
+const num1=new Number(200);
+console.log(num1);
+console.log(num1.toString());
+console.log(num1.toString().length);
+console.log(num1.toFixed)
+ 
+const num2=1124.2266;
+console.log(num2.toPrecision(5));
