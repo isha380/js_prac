d7e033e9859e1fc53f89dfb493a1e14259a3a7ba
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-09 01:39:47 +0000

    math and string

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -8,3 +8,10 @@ console.log(num1.toFixed)
  
 const num2=1124.2266;
 console.log(num2.toPrecision(5));
+//+++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++=
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
