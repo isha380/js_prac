a2b54113766def3fd63cba20e32915a5776d2d1b
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-11 14:31:50 +0000

    dates in js

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,10 @@
+//dates
+let todaydate =new Date();
+console.log(todaydate);
+console.log(todaydate.toDateString());
+console.log(todaydate.toLocaleString);
+console.log(typeof todaydate);
+
+let createMyDate=new Date("2080-12-29");
+console.log(createMyDate);
+console.log(createMyDate.toLocaleDateString);
