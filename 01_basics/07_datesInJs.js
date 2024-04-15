bbffd05b3041e96230630d48644c9b98a8864e28
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-15 09:52:41 +0000

    date

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -3,8 +3,10 @@ let todaydate =new Date();
 console.log(todaydate);
 console.log(todaydate.toDateString());
 console.log(todaydate.toLocaleString);
-console.log(typeof todaydate);
+console.log(todaydate.toISOString);
+console.log(todaydate.toJSON);
+// console.log(typeof todaydate);
 
-let createMyDate=new Date("2080-12-29");
-console.log(createMyDate);
-console.log(createMyDate.toLocaleDateString);
+// let createMyDate=new Date("2080-12-29");
+// console.log(createMyDate);
+// console.log(createMyDate.toLocaleDateString);
