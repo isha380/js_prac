ba8b9c874df2c9d76d8976e12262ddb8153dd9fa
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-15 09:57:58 +0000

    prototype of date practice

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -2,9 +2,9 @@
 let todaydate =new Date();
 console.log(todaydate);
 console.log(todaydate.toDateString());
-console.log(todaydate.toLocaleString);
-console.log(todaydate.toISOString);
-console.log(todaydate.toJSON);
+console.log(todaydate.toLocaleString());
+console.log(todaydate.toISOString());
+console.log(todaydate.toJSON());
 // console.log(typeof todaydate);
 
 // let createMyDate=new Date("2080-12-29");
