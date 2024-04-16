cec183fb025e8d251d71c2cfa0028d7ea927693c
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-16 13:14:54 +0000

    dates practice

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -1,12 +1,25 @@
 //dates
-let todaydate =new Date();
-console.log(todaydate);
-console.log(todaydate.toDateString());
-console.log(todaydate.toLocaleString());
-console.log(todaydate.toISOString());
-console.log(todaydate.toJSON());
+// let todaydate =new Date();
+// console.log(todaydate);
+// console.log(todaydate.toDateString());
+// console.log(todaydate.toLocaleString());
+// console.log(todaydate.toISOString());
+// console.log(todaydate.toJSON());
 // console.log(typeof todaydate);
 
 // let createMyDate=new Date("2080-12-29");
 // console.log(createMyDate);
 // console.log(createMyDate.toLocaleDateString);
+
+let myCreateDate= new Date(2024,0,16);
+console.log(myCreateDate.toLocaleDateString());
+console.log(myCreateDate.toDateString());
+
+let myCreateDate1= new Date(2024,0,16,6,55);
+console.log(myCreateDate1.toLocaleString());
+
+let datee= new Date("2024-4-16");
+console.log(datee.toLocaleString());
+
+let date2= new Date("01-14-2024");
+console.log(date2.toLocaleString());
