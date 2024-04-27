2c223d1dc236078088e739ccbd741ef1590e57bd
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-27 01:27:25 +0000

    learn shift , join

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,3 +1,35 @@
-const Array=[45,4,5,3];
-const mhero=["shaktiman","mom"];
-console.log(Array[1]);
+const Array=[1,2,3,4];
+// const mhero=["shaktiman","mom"];
+// console.log(Array[1]);
+
+//array methods
+
+// Array.push(100);
+// Array.push(99);
+// Array.push(88);
+// console.log(Array);
+
+// Array.pop();
+// console.log(Array);
+
+//unshift adds value from the start that causes to shift all the array 
+// Array.unshift(44);
+//we give argument in shift but not in shift
+// console.log(Array); 
+
+Array.shift();
+console.log(Array);
+console.log(Array.includes(66));
+
+console.log(Array.indexOf(4));
+
+//join changes the type of array. (adds all element into string) 
+const newArray =Array.join();
+
+console.log(Array);
+console.log(newArray);
+console.log(typeof newArray);
+
+//slice, splice
+
+
