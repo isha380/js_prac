d96925dcebd90d384587694f8aa22f33b8be5947
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-28 11:19:09 +0000

    use of slice

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,4 @@
-const Array=[1,2,3,4];
+const Array=[0,1,2,3,4,5];
 // const mhero=["shaktiman","mom"];
 // console.log(Array[1]);
 
@@ -18,18 +18,22 @@ const Array=[1,2,3,4];
 // console.log(Array); 
 
 Array.shift();
-console.log(Array);
-console.log(Array.includes(66));
+// console.log(Array);
+// console.log(Array.includes(66));
 
-console.log(Array.indexOf(4));
+// console.log(Array.indexOf(4));
 
 //join changes the type of array. (adds all element into string) 
 const newArray =Array.join();
 
-console.log(Array);
-console.log(newArray);
-console.log(typeof newArray);
+// console.log(Array);
+// console.log(newArray);
+// console.log(typeof newArray);
 
 //slice, splice
 
-
+console.log("A",Array);
+console.log("hi ");
+const Array1 = Array.slice(1,4);
+console.log(Array1);
+//first is included but not 
