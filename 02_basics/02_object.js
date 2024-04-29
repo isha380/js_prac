333116ddf02261caa7c3b3ca5edfe16a45d0830e
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-29 13:40:51 +0000

    freeze, function info

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,3 +1,37 @@
 //singleton
+//when constructore is made for eg: Object.create
 
 //object literals
+const mysymb= Symbol("key1");
+const developer={
+    name:"isha",
+    "full name": "isha sithung magar",
+    [mysymb] :"1_key",
+    age:88,
+    location:"bhaktapur",
+    email:"isha@gmail.com",
+    isLoggedIn:false,
+
+}
+// console.log(developer["email"]);
+// console.log(developer[mysymb]);
+// console.log(developer.location);
+//note: access the value usually by using dot(.) and use the square bracket [] where we can't use dot
+
+// developer.email ="isha23@gmail.com";
+// Object.freeze(developer);
+
+// developer.email="hitesh@gmail.com";
+// console.log(developer);
+
+//we can treat the function as a variable in javascript 
+ 
+developer.greetings = function (){
+    console.log("hello user. keep working hard!!");
+}
+console.log(developer.greetings());
+
+developer.greetings2 = function (){
+    console.log(`hello user, ${this.name} ${this.location}`);
+}
+console.log(developer.greetings2());
