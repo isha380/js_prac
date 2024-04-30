7b1ecb561f705fa4560122662e0b7ccd7a6b36e1
isha magar <140723992+isha380@users.noreply.github.com>
2024-04-30 23:45:36 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -4,3 +4,9 @@ const spotifyUser = new Object()
 console.log(spotifyUser);
 const User={}
 console.log(User);
+
+spotifyUser.id="324jdk";
+spotifyUser.name="isha";
+spotifyUser.isLoggedIn =false;
+
+console.log(spotifyUser);
