d31cffba950b6234a13dd934946070e0092b79ef
isha magar <140723992+isha380@users.noreply.github.com>
2024-05-01 23:22:57 +0000

    Objectg. assign method

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -10,3 +10,29 @@ spotifyUser.name="isha";
 spotifyUser.isLoggedIn =false;
 
 console.log(spotifyUser);
+
+const regularuser={
+    email:"isha@gmail.com",
+    fullname:{
+        userName:{
+            firstName:"isha",
+            lastName:"magar"
+        }
+    }
+
+}
+
+console.log(regularuser.fullname.userName);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+const obj3=Object.assign(obj1,obj2);
+console.log(obj3);
+const obj4=Object.assign({},obj1,obj2);
+console.log(obj4);
+
+//after spreading and combinig 
+
+const obj5={...obj1,...obj2};
+console.log(obj5);
