43b4b0a877aafcec731d92242241707cb4c93159
isha magar <140723992+isha380@users.noreply.github.com>
2024-05-02 23:07:13 +0000

    object key,values, entries discuss

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,15 +1,15 @@
 //singleton
 
 const spotifyUser = new Object()
-console.log(spotifyUser);
+// console.log(spotifyUser);
 const User={}
-console.log(User);
+// console.log(User);
 
 spotifyUser.id="324jdk";
 spotifyUser.name="isha";
 spotifyUser.isLoggedIn =false;
 
-console.log(spotifyUser);
+// console.log(spotifyUser);
 
 const regularuser={
     email:"isha@gmail.com",
@@ -22,17 +22,50 @@ const regularuser={
 
 }
 
-console.log(regularuser.fullname.userName);
+// console.log(regularuser.fullname.userName);
 
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 
 const obj3=Object.assign(obj1,obj2);
-console.log(obj3);
+// console.log(obj3);
 const obj4=Object.assign({},obj1,obj2);
-console.log(obj4);
+// console.log(obj4);
 
 //after spreading and combinig 
 
 const obj5={...obj1,...obj2};
-console.log(obj5);
+// console.log(obj5);
+
+const users =[
+    {
+        id:8,
+        name:"isha"
+    },
+    {
+        id:9,
+        name:"isha"
+    },
+    {
+        id:10,
+        name:"isha"
+    }
+]
+users[1].name;
+console.log(spotifyUser);
+console.log(Object.keys(spotifyUser));
+console.log(Object.values(spotifyUser));
+console.log(Object.entries(spotifyUser));
+//entries ma key value lai array ma rakheko hunx
+
+console.log(spotifyUser.hasOwnProperty('name'));
+console.log(spotifyUser.hasOwnProperty('isLogged'));
+
+
+const course ={
+    
+        courseName:string;
+        price: string ;
+        courdeInstruction:string;
+  
+}
