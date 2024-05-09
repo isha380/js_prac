1615b99cd12b60c06b4b367d8993b7c8fefd8255
isha magar <140723992+isha380@users.noreply.github.com>
2024-05-09 14:37:04 +0000

    browser (engine)

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -2,7 +2,14 @@ const user ={
     username:"isha",
     price:888,
     welcomeMessage: function(){
-        console.log
-        
+        console.log(`${this.username},hello world`);
+        console.log(this);
     }
 }
+user.welcomeMessage();
+user.username="sanju";
+user.welcomeMessage();
+console.log(this);
+
+// browser ko bitra run garne engine ko dherai jaso,  the global object inside browser i.e window object that is mostly used
+
