b12c8708a4e1e0ad23b0c86d0beefb6faa9c1d63
isha magar <140723992+isha380@users.noreply.github.com>
2024-05-10 14:41:01 +0000

    learning Arrow function

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -2,14 +2,51 @@ const user ={
     username:"isha",
     price:888,
     welcomeMessage: function(){
-        console.log(`${this.username},hello world`);
-        console.log(this);
+        // console.log(`${this.username},hello world`);
+        // console.log(this);
     }
 }
 user.welcomeMessage();
 user.username="sanju";
 user.welcomeMessage();
-console.log(this);
+// console.log(this);
 
 // browser ko bitra run garne engine ko dherai jaso,  the global object inside browser i.e window object that is mostly used
 
+// -----------------------------------------------------------------------------------------------------------------------------
+ const momo= function(){
+    let username= "paneer momo";
+    console.log(this.username);
+ }
+ console.log(this.username);
+
+ // -----------------------------------------------------------------------------------------------------------------------------
+
+function disco(){
+    console.log(this);
+}
+disco();
+
+// -----------------------------------------------------------------------------------------------------------------------------
+const momo2 =() =>{
+    let username= "paneer momo";
+    console.log(this);
+}
+momo2();
+
+// -----------------------------------------------------------------------------------------------------------------------------
+// explicit return
+const add=(num1,num2) =>{
+    return num1+num2;
+}
+console.log(add(4,4));
+
+// -----------------------------------------------------------------------------------------------------------------------------
+//implicit return
+const add1=(num1,num2) =>(num1+num2)
+console.log(add(4,6));
+
+// -----------------------------------------------------------------------------------------------------------------------------
+// object return
+const object=() =>({username:"isha sithung"})
+console.log(object());
